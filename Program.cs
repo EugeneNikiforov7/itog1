d59e3b2ed1a0b50cd81013335985589e09ebaf74
EugeneNikiforov <eugene_vn@mail.ru>
2023-09-11 16:07:03 +0500

    add getTextArrayNewSize

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿
+﻿int getTextArrayNewSize(string[] arr)
+{
+int indexNew = 0;
+foreach (string s in arr)
+{
+    if (s.Length <= 3)
+    {
+        indexNew += 1;
+    }
+}
+return indexNew;
+}
+
+
+
 string[] textArray = {"Hello", "2", "world", ":-)", "sdf"};
