7acdcaa5a8bc497b755f2f4fea4ed727f9124ad5
EugeneNikiforov <eugene_vn@mail.ru>
2023-09-11 16:11:21 +0500

    end

--- Program.cs
+++ Program.cs
@@ -1,16 +1,41 @@
 ﻿int getTextArrayNewSize(string[] arr)
 {
-int indexNew = 0;
-foreach (string s in arr)
-{
-    if (s.Length <= 3)
+    int indexNew = 0;
+    foreach (string s in arr)
     {
-        indexNew += 1;
+        if (s.Length <= 3)
+        {
+            indexNew += 1;
+        }
+    }
+    return indexNew;
+}
+
+string[] CreateNewArray(int indexNew, string[] arr)
+{
+    Console.WriteLine(indexNew);
+    string[] textArrayNew = new string[indexNew];
+    indexNew = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            textArrayNew[indexNew] = arr[i];
+            indexNew += 1;
+        }
+    }
+    return textArrayNew;
+}
+
+void TestNewArray(string[] arr)
+{
+    Console.WriteLine();
+    foreach (string s in arr)
+    {
+        Console.WriteLine(s);
     }
 }
-return indexNew;
-}
 
-
-
-string[] textArray = {"Hello", "2", "world", ":-)", "sdf"};
+string[] textArray = { "Hello", "2", "world", ":-)", "sdf" };
+string[] textArrayNew = CreateNewArray(getTextArrayNewSize(textArray), textArray);
+TestNewArray(textArrayNew);
